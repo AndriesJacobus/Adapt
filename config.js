52b67c01395caf7d5c7d0f3e3f18bd13b58f8e59
Denham Preen <denhampreen@gmail.com>
2017-10-14 17:09:52 +0200

    basic config file start

---
+++ config.js
@@ -0,0 +1,24 @@
+var healthCareCourse = {
+    healthCare : "Health Care",
+    Careers: {
+        nurse: "Nurse",
+        pharmacist: "Pharmacist",
+        healthWorker: "Health Care Worker"
+    }
+}
+
+var foodAndBeverageCourse = {
+    foodAndBeverage : "Food and Beverage",
+    Careers: {
+        waiter: "Waiter",
+        manager: "Manager",
+        chef: "Chef"
+    }
+}
+
+var india = [healthCareCourseCareers, foodAndBeverageCourseCareers];
+
+//for (int x, india.lentgth ){
+//    for (healthCareCourse.Careers.length)
+//}
+//     
